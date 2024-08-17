75e39155719488b6a4b75e8dda009c46d9f3018e
anushome09 <anushome081101@gmail.com>
2024-08-17 19:14:37 +0530

    Employee Class using Dapper

---
+++ Program/EmployeeDapper.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Tutorial
+{
+    public class EmployeeDapper
+    {
+        string connectionString = "Server = (localdb)\\MSSQLLocalDB; Database = Local Database; Integrated Security = true";
+        SqlConnection connection = null;
+
+        public EmployeeDapper()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        // Query to insert data into the database using dapper
+        public void insertQueryFunction(Employee employee)
+        {
+            // SQL Query to insert new elements
+            string insertQuery = "INSERT INTO Employee(Id, Name, Age, Salary) VALUES(@Id, @Name, @Age, @Salary)";
+
+            // executing the query
+            connection.Query(insertQuery, employee);
+        }
+
+        // Query to fetch data from the database
+        public List<Employee> fetchQueryFunction()
+        {
+            // SQL Query to fetch data
+            string fetchQuery = "SELECT * FROM Employee";
+
+            List<Employee> employees = connection.Query<Employee>(fetchQuery).ToList();
+
+            return employees;
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Dapper_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDapper dapper = new EmployeeDapper();
+
+            // An object of employee containing the data
+            Employee objEmployee = new Employee
+            {
+                Id = 128734,
+                Name = "Jane Doe",
+                Age = 25,
+                Salary = 900000
+            };
+
+            // Insert data into the table
+            dapper.insertQueryFunction(objEmployee);
+
+            // Fetch Data from the database
+            var EmployeeList = dapper.fetchQueryFunction();
+        }
+    }
+}
